234d5d4c1547dd07f225b1544045bb79618cfa59
Andres Partida <andrespartida@mbp-de-erick.gateway.2wire.net>
2014-10-28 23:08:11 -0600

    Initial Commit

---
+++ ProyectoFinalView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProyectoFinalView
+//
+//  Created by Andres Partida on 28/10/14.
+//  Copyright (c) 2014 Andres Partida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProyectoFinalView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProyectoFinalView
+//
+//  Created by Andres Partida on 28/10/14.
+//  Copyright (c) 2014 Andres Partida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
